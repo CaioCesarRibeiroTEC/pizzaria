e594b4e069ba3a32e1b82e9f7d43d6491307ec35
Caio Cesar Ribeiro <caio.ribeiro_dev.93@outlook.com>
2024-09-20 14:53:39 -0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,98 @@
+let pizzaJson = [
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'assets/pizza.png',
+        price: [30.00, 40.00, 50.00],
+        sizes: [
+          '4 fatias',
+          '6 fatias',
+          '8 fatias'
+        ],
+        description: 'Molho de tomate, camada dupla de mussarela e orégano'
+    },
+    {
+    id: 2,
+    name: 'Calabresa',
+    img: 'assets/pizza2.png',
+    price: [30.00, 40.00, 50.00],
+    sizes: [
+        '4 fatias',
+        '6 fatias',
+        '8 fatias'
+    ],
+    description: 'Molho de tomate, muçarela, calabresa fatiada, cebola e orégano'
+    },
+    {
+    id: 3,
+    name: 'Quatro Queijos',
+    img: 'assets/pizza3.png',
+    price: [30.00, 40.00, 50.00],
+    sizes: [
+        '4 fatias',
+        '6 fatias',
+        '8 fatias'
+    ],
+    description: 'Molho de tomate, camadas de muçarela, provolone, parmessão, gorgonzola e orégano'
+    },
+    {
+    id: 4,
+    name: 'Brasileira',
+    img: 'assets/pizza4.png',
+    price: [30.00, 40.00, 50.00],
+    sizes: [
+        '4 fatias',
+        '6 fatias',
+        '8 fatias'
+    ],
+    description: 'Molho de tomate, muçarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+    },
+    {
+    id: 5,
+    name: 'Portuguesa',
+    img: 'assets/pizza5.png',
+    price: [30.00, 40.00, 50.00],
+    sizes: [
+        '4 fatias',
+        '6 fatias',
+        '8 fatias'
+    ],
+    description: 'Molho de tomate, muçarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+    },
+    {
+    id: 6,
+    name: 'Moda da Casa',
+    img: 'assets/pizza6.png',
+    price: [30.00, 40.00, 50.00],
+    sizes: [
+        '4 fatias',
+        '6 fatias',
+        '8 fatias'
+    ],
+    description: 'Molho de tomate, muçarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+    },
+    {
+    id: 7,
+    name: 'Banana com canela',
+    img: 'assets/pizza7.png',
+    price: [30.00, 40.00, 50.00],
+    sizes: [
+        '4 fatias',
+        '6 fatias',
+        '8 fatias'
+    ],
+    description: 'muçarela, banana, canela e açúcar'
+    },
+    {
+    id: 8,
+    name: 'Chocolate com morango',
+    img: 'assets/pizza8.png',
+    price: [30.00, 40.00, 50.00],
+    sizes: [
+        '4 fatias',
+        '6 fatias',
+        '8 fatias'
+    ],
+    description: 'Creme de leite, lascas de chocolate e morangos'
+    }
+];
